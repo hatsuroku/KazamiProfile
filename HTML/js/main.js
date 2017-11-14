2b5fdc54a524565c6e2508e78f8b97f37f01e781
hatsuroku <1127489497@qq.com>
2017-11-14 20:33:34 +0800

    用jQuery库重写了侧边栏切换的动画，并且去除侧边栏背景的transition

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,5 +1,63 @@
-/* 后来自己调用别人动画库实现的点击效果 */
+/* 尝试用jQuery库来实现 */
 var isDangan = false;
+
+function changeStyle() {
+    if(isDangan) {
+        $("#avatar").attr("src","../img/dora_kid.jpg");
+        $("#myName").text("Kazami Hatsuroku");
+        $("#author").text("By Kazami Hatsuroku 2017");
+        $("#mainLeft").css({
+            "color" : "white",
+            "backgroundColor" : "#1467a3",
+        });
+        $(".mae a").css({
+            "color": "white",
+        });
+        $("#hiddenLink").css({
+            "color": "transparent",
+            "transform": "none",
+        });
+        isDangan = false;
+    }
+    else {
+        $("#avatar").attr("src","../img/monokuma.jpg");
+        $("#myName").text("Ryukamine Yuuji");
+        $("#author").text("By Ryukamine Yuuji 2017");
+        $("#mainLeft").css({
+            "color" : "black",
+            "backgroundColor" : "#ff01cc",
+        });
+        $(".mae a").css({
+            "color": "black",
+        });
+        $("#hiddenLink").css({
+            "color": "transparent",
+            "transform": "none",
+        });
+        isDangan = true;
+    }
+}
+
+
+$(document).ready(function () {
+    $("#avatar").click(function () {
+        $("#mainLeft")
+            .animate({
+                right : '500px',
+            }, 1000, changeStyle)
+            .animate({
+                right: "0px",
+            }, 1000);
+        });
+});
+
+
+
+
+
+
+/* 后来自己调用别人动画库实现的点击效果 */
+/*var isDangan = false;
 var leftFrame = document.getElementById('mainLeft');
 var leftTable = document.getElementById('leftTable');
 var ava = document.getElementById('avatar');
@@ -30,7 +88,7 @@ function changeDanganColor() {
 function changeSituation() {
     if (isDangan) {
         myName.innerHTML = "Kazami Hatsuroku";
-        author.innerHTML = "by Kazami Hatsuroku 2017"
+        author.innerHTML = "by Kazami Hatsuroku 2017";
         leftFrame.className = 'left whiteFont changeBackColor shiftDangan mae';
         leftFrame.style.backgroundColor = "#1467A3";
         leftFrame.style.color = "#FFFFFF";
@@ -41,7 +99,7 @@ function changeSituation() {
     }
     else {
         myName.innerHTML = "Ryukamine Yuuji";
-        author.innerHTML = "by Ryukamine Yuuji 2017"
+        author.innerHTML = "by Ryukamine Yuuji 2017";
         leftFrame.className = 'left whiteFont changeBackColor shiftDangan ato';
         leftFrame.style.color = "#000000";
         ava.src = "../img/monokuma.jpg";
@@ -58,14 +116,15 @@ function slideIn() {
         }, {
             type: dynamics.easeInOut,
         });
-}
+}*/
 
 
 
 
 
 /* 先前自己写的变化效果 */
-/* var isDangan = false;
+/*
+var isDangan = false;
 var leftFrame = document.getElementById('mainLeft');
 var ava = document.getElementById('avatar');
 var myName = document.getElementById('myName');
